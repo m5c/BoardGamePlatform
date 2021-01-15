ea060cc9a4b9ff6252aba1df2257568a92c4b67d
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2021-01-15 15:08:37 -0500

    modified update skript for auto pull after update

--- updatesubmodules.sh
+++ updatesubmodules.sh
@@ -6,17 +6,20 @@ BASE=$(pwd)
 cd LobbyService
 git branch
 git checkout master
+git pull
 
 # update LS webui
 cd src/main/resources/static
 git branch
 git checkout main
+git pull
 
 # update Xox
 cd $BASE
 cd LobbyServiceXox
 git branch
 git checkout main
+git pull
 
 cd $BASE
 
